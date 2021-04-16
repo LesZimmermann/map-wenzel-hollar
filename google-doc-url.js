0a01a11398f0420f4b6d7ff0b11104203e752a52
Leslie P. Zimmermann <52155542+LesZimmermann@users.noreply.github.com>
2021-04-16 16:37:17 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ShXBjOyVmSiB4OW9Kqpds5lhFxjPQHVUpyR4zyu4YxU/edit#gid=1005314818';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyBahrwRP-YkGdZ0ZGp46ashMuKCIqMUfd4';
